7d4c5ce9886b467c2a54e1b0e4b767ab28e113fe
Beatriz <beatrizcarmoperez@gmail.com>
2024-04-14 23:56:55 -0300

    ajustes

--- config/sequelize-cli.js
+++ config/sequelize-cli.js
@@ -1,7 +1,18 @@
 //variáveis de ambientes
-require('dotenv').config();
+// require('dotenv').config();
+
+// module.exports = {
+//     dialect: 'postgres',
+//     url: process.env.DATABASE_URL
+// }
 
 module.exports = {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     url: process.env.DATABASE_URL
 }
