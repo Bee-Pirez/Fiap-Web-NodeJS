79a2d034af6f98d659962374383d0f7e2ef95a74
Beatriz <beatrizcarmoperez@gmail.com>
2024-04-15 20:52:52 -0300

    ajustes

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 
+
 import express from "express";
 import { router } from "./routes";
 
 const app = express();
 
-// Habilita o CORS para todas as origens
-app.use(cors());
+// // Habilita o CORS para todas as origens
+// app.use(cors());
+const corsConfig = {
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 
 app.use(express.json());
 
